c20e2c6573bb08dbc16111b1ecdfbcafd76a0f83
lfsanchez <luislucho86@gmail.com>
2018-08-27 20:53:44 -0500

    adicion de componente menu

--- reciclaje/src/app/app.module.ts
+++ reciclaje/src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppmenuComponent } from './appmenu/appmenu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppmenuComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    AppmenuComponent
+  ]
 })
 export class AppModule { }
